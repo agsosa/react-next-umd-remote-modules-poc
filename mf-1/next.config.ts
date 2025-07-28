500c3c4faa26a537d2b9c3623c084d3373a42415
Alejandro Sosa <aleej.gs@gmail.com>
2025-07-28 13:04:50 -0300

    Add cache control to mf1 production builds

--- mf-1/next.config.ts
+++ mf-1/next.config.ts
@@ -1,16 +1,34 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 
+  ...(isProd && {
+    async headers() {
+      return [
+        {
+          source: "/remote-modules/:path*",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=300, stale-while-revalidate=86400",
+            },
+          ],
+        },
+      ];
+    },
+  }),
+
   // ignore eslint/typescript for now, this is just a PoC
   eslint: {
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true
-  }
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
