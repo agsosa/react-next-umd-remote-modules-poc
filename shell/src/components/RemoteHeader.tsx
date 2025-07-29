03db740ad29dbd9903df734cbeb42936d0e71a2c
Alejandro Sosa <aleej.gs@gmail.com>
2025-07-28 21:01:25 -0300

    Update RemoteHeader.tsx

--- shell/src/components/RemoteHeader.tsx
+++ shell/src/components/RemoteHeader.tsx
@@ -1,31 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import { useRemoteModule } from "../hooks/useRemoteModule";
 import type { HeaderProps } from "../types/mf1-remote-components";
 import getConfig from "next/config";
 
-function useTraceUpdate(props) {
-  const prev = useRef(props);
-  useEffect(() => {
-    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
-      if (prev.current[k] !== v) {
-        ps[k] = [prev.current[k], v];
-      }
-      return ps;
-    }, {});
-    if (Object.keys(changedProps).length > 0) {
-      console.log('Changed props:', changedProps);
-    }
-    prev.current = props;
-  });
-}
-
-
 const RemoteHeader = () => {
   const {
     publicRuntimeConfig: { MF1_BASE_URL },
   } = getConfig();
-  
+
   const router = useRouter();
 
   const {
@@ -38,7 +21,6 @@ const RemoteHeader = () => {
     componentName: "Header",
   });
 
-  useTraceUpdate({Header, loading, error })
   if (loading) return <p>Loading...</p>;
 
   if (error) {
@@ -51,7 +33,6 @@ const RemoteHeader = () => {
   const handleMount = () => {
     console.log("Shell App: El header remoto montó en la app!");
   };
-  console.log("renderrender")
 
   return <Header appName="Shell App" router={router} onMount={handleMount} />;
 };
